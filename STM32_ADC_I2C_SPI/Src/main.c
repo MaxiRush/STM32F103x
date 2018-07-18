443b967be2b5e1f148d34159ad1cf0499116da3e
MaxiRush <longostemo@gmail.com>
2018-07-18 19:38:03 +0300

    Уменьшил задержку до 100 мс

--- STM32_ADC_I2C_SPI/Src/main.c
+++ STM32_ADC_I2C_SPI/Src/main.c
@@ -115,7 +115,7 @@ int main(void)
   while (1)
   {
       HAL_GPIO_TogglePin (GPIOC, GPIO_PIN_13);
-      HAL_Delay(500);
+      HAL_Delay(100);
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
